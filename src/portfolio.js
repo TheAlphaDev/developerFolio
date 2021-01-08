67832f6f36d3bd8830177e04b3697fcae81d18c5
AlphaBeta <76069950+TheAlphaDev@users.noreply.github.com>
2021-01-08 10:34:14 -0800

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,26 +9,26 @@ const illustration = {
 };
 
 const greeting = {
-  username: 'Saad Pasta',
-  title: "Hi all, I'm Saad",
+  username: 'AlphaDev',
+  title: "Hello, I am AlphaDev",
   subTitle: emoji(
-    'A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks.'
+    'A passionate web developer who loves node-js.'
   ),
   resumeLink:
-    'https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing',
+    'https://perpetual.pizza',
   displayGreeting: true, // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: 'https://github.com/saadpasta',
-  linkedin: 'https://www.linkedin.com/in/saadpasta/',
-  gmail: 'saadpasta70@gmail.com',
-  gitlab: 'https://gitlab.com/saadpasta',
-  facebook: 'https://www.facebook.com/saad.pasta7',
-  medium: 'https://medium.com/@saadpasta',
-  stackoverflow: 'https://stackoverflow.com/users/10422806/saad-pasta',
+  github: 'https://github.com/TheAlphaDev',
+  linkedin: 'https://perpetual.pizza',
+  gmail: 'alphadev.me@gmail.com',
+  gitlab: 'https://gitlab.com/TheAlphaDev',
+  facebook: 'https://perpetual.pizza',
+  medium: 'https://perpetual.pizza',
+  stackoverflow: 'https://perpetual.pizza',
   // Instagram and Twitter are also supported in the links!
   display: true, // Set true to display this section, defaults to false
 };
@@ -37,15 +37,11 @@ const socialMediaLinks = {
 
 const skillsSection = {
   title: 'What I do',
-  subTitle: 'CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK',
+  subTitle: 'Crazy node-js dev.',
   skills: [
     emoji(
-      '⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications'
-    ),
-    emoji('⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks'),
-    emoji(
-      '⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean'
-    ),
+      '⚡ Develop websites.'
+    )
   ],
 
   /* Make Sure to include correct Font Awesome Classname to view your icon
@@ -69,16 +65,8 @@ https://fontawesome.com/icons?d=gallery */
       fontAwesomeClassname: 'fab fa-js',
     },
     {
-      skillName: 'reactjs',
-      fontAwesomeClassname: 'fab fa-react',
-    },
-    {
       skillName: 'nodejs',
       fontAwesomeClassname: 'fab fa-node',
-    },
-    {
-      skillName: 'swift',
-      fontAwesomeClassname: 'fab fa-swift',
     },
     {
       skillName: 'npm',
@@ -89,21 +77,9 @@ https://fontawesome.com/icons?d=gallery */
       fontAwesomeClassname: 'fas fa-database',
     },
     {
-      skillName: 'aws',
-      fontAwesomeClassname: 'fab fa-aws',
-    },
-    {
-      skillName: 'firebase',
-      fontAwesomeClassname: 'fas fa-fire',
-    },
-    {
       skillName: 'python',
       fontAwesomeClassname: 'fab fa-python',
-    },
-    {
-      skillName: 'docker',
-      fontAwesomeClassname: 'fab fa-docker',
-    },
+    }
   ],
   display: true, // Set false to hide this section, defaults to true
 };
@@ -114,8 +90,8 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: 'Harvard University',
-      logo: require('./assets/images/harvardLogo.png'),
+      schoolName: 'Willows Prep',
+      logo: '',
       subHeader: 'Master of Science in Computer Science',
       duration: 'September 2017 - April 2019',
       desc: 'Participated in the research of XXX and published 3 papers.',
@@ -123,16 +99,7 @@ const educationInfo = {
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
       ],
-    },
-    {
-      schoolName: 'Stanford University',
-      logo: require('./assets/images/stanfordLogo.png'),
-      subHeader: 'Bachelor of Science in Computer Science',
-      duration: 'September 2013 - April 2017',
-      desc:
-        'Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...',
-      descBullets: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit'],
-    },
+    }
   ],
 };
 
